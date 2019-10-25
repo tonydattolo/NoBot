2a49ba93726f45b013230a6f418764b5eab869c6
TonyTargaryen <tdattolo@iu.edu>
2019-10-24 20:39:21 -0400

    working on setting up reading in codes and defining them as variables

--- secret.py
+++ secret.py
@@ -1,8 +1,9 @@
 import os
 
 # retrieve keys from environment variables
-consumer_key = os.environ['TWBOT_CON_KEY']
-consumer_secret = os.environ['TWBOT_CON_SECRET']
-access_token = os.environ['TWBOT_ACCESS_TOKEN']
-access_secret = os.environ['TWBOT_ACCESS_SECRET']
-handle = os.environ['TWBOT_HANDLE']
+# consumer_key = os.environ['TWBOT_CON_KEY']
+# consumer_secret = os.environ['TWBOT_CON_SECRET']
+# access_token = os.environ['TWBOT_ACCESS_TOKEN']
+# access_secret = os.environ['TWBOT_ACCESS_SECRET']
+# handle = os.environ['TWBOT_HANDLE']
+
